8f093233324af964488aeccd99052cced166159d
sahsharma <sharmasahil2103@gmail.com>
2025-06-26 23:27:24 +0530

    Connect to mongoo atlas

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' }); 
+
+import connectDB from "./db/index.js";
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+
+const app=express()
+
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listion in port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
