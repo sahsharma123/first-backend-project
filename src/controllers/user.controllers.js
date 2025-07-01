2d51e890fee9a8ca004685cbe59efdead24775f8
sahsharma <sharmasahil2103@gmail.com>
2025-07-02 00:12:41 +0530

    add register controller and bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/clodinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser =asyncHandler(async (req,res)=>{
    //get user details from frontent
@@ -12,7 +16,67 @@ const registerUser =asyncHandler(async (req,res)=>{
     //return res
 
     const {fullName,email,username,password} =req.body
+    console.log("email: ",email);
 
+    //you can validate like simple if and can use some function
+
+    // if(fullName===""){
+    //     throw new ApiError(400,"fullName is required")
+    // }
+
+    if(
+        [fullName,email,username,password].some((field)=>
+        field?.trim()==="")
+    ){
+        throw new ApiError(400,"All field are required")
+    }
+
+    const existedUser=User.findOne({
+        $or:[{ username },{ email }]
+    })
+    if(existedUser){
+        throw new ApiError(409,"User with email or user already exist")
+    }
+
+    //req.body mein sara data ata hai
+    //multer hume req.files ka access deta hi
+
+    //? it is used if req.file is undefine or null then it prevent crashing
+
+    const avatarLocalPath=req.files?.avatar[0]?.path;
+    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required")
+    }
+    const avatar=await uploadOnCloudinary(avatarLocalPath)
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400,"Avatar file is required")
+    }
+
+    const user=await User.create({
+        fullName,
+        avatar: avatar.url,
+
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+
+    })
+    const createdUser=await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"somthing went wrong while registring user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+    )
 
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router =Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser//this mehtod is executing and upload.field is creating middlewre
+)
 
 
 export default router
